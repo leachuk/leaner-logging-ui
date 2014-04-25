85ffe9ec0172916f7c21931b7edc8c5a6082f7f5
leachuk <stew.leach@gmail.com>
2014-04-25 22:45:55 +1000

    integrating polylines into map.
    rough first cut of adding polylines.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -30,15 +30,45 @@ learnerLogCtrl.controller('logDetailCtrl', ['$scope', '$rootScope', '$routeParam
 	$scope.testvarslogdetail = ['detailvar1','detailvar2'];
 	$scope.logItemId = $routeParams.logid;
 	var itemData = $filter('filter')($rootScope.logData, {id: $routeParams.logid});
+	var runOnceSet = false;
 	$scope.logItemData = itemData[0];
 	$scope.mapOptions = {
-      center: new google.maps.LatLng(35.784, -78.670),
-      zoom: 15,
+      center: new google.maps.LatLng(-33.8738362136655, 151.18457794189453),
+      zoom: 12,
       mapTypeId: google.maps.MapTypeId.ROADMAP
     };
-	console.log("controller: logDetailCtrl");
-	console.log($routeParams);
-	console.log($rootScope.logData);
-	console.log(itemData);
+    console.log("controller: logDetailCtrl");
+    //console.log(itemData.coords);
+	//console.log($routeParams);
+	//console.log($rootScope.logData);
+	//console.log(itemData);
+    var mapPolyLine = [];
+    for (var i=0; i < itemData[0].coords.length; i++){
+    	var lat = itemData[0].coords[i].lat;
+    	var lon = itemData[0].coords[i].lon;
+    	var latLon = new google.maps.LatLng(lat,lon);
+    	mapPolyLine.push(latLon);
+    }
+ 
+  	$scope.onMapIdle = function() {
+        if (runOnceSet === false){
+            new google.maps.Marker({
+                map: $scope.myMap,
+                position: new google.maps.LatLng(-33.8738362136655, 151.18457794189453)
+            });    
+       
+        	new google.maps.Polyline({
+				map: $scope.myMap,
+				path: mapPolyLine,
+				geodesic: true,
+				strokeColor: '#000FFF',
+				strokeOpacity: 1.0,
+				strokeWeight: 2
+			});
+			runOnceSet = true;
+			console.log("run once idle");
+        }  
+    };
+      	
 }]);
 
